b1cca1030c533475c11425c6e230fcaf0405d0c9
Octavian Mitrica <octavian.mitrica4@gmail.com>
2023-03-31 00:39:38 +0300

    initial_bottom_nav

--- app/src/main/java/com/example/socializer/MainActivity.kt
+++ app/src/main/java/com/example/socializer/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.socializer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+<<<<<<< Updated upstream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+=======
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.socializer.databinding.ActivityLogInBinding
+import com.example.socializer.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.button.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent);
+        }
+
+        val navContainerFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as  NavHostFragment
+        navController = navContainerFragment.navController
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+
+        setupWithNavController(bottomNavView, navController)
+>>>>>>> Stashed changes
     }
 }
